1fceee6ff4288ce0930bab24c2e1b67683c8505f
Haoran Zhou <kens0serious@gmail.com>
2021-11-30 02:40:20 -0500

    Done

--- 08-menu/app.js
+++ 08-menu/app.js
@@ -1,74 +1,144 @@
 const menu = [
   {
     id: 1,
-    title: "buttermilk pancakes",
-    category: "breakfast",
+    title: 'buttermilk pancakes',
+    category: 'breakfast',
     price: 15.99,
-    img: "./images/item-1.jpeg",
+    img: './images/item-1.jpeg',
     desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
   },
   {
     id: 2,
-    title: "diner double",
-    category: "lunch",
+    title: 'diner double',
+    category: 'lunch',
     price: 13.99,
-    img: "./images/item-2.jpeg",
+    img: './images/item-2.jpeg',
     desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
   },
   {
     id: 3,
-    title: "godzilla milkshake",
-    category: "shakes",
+    title: 'godzilla milkshake',
+    category: 'shakes',
     price: 6.99,
-    img: "./images/item-3.jpeg",
+    img: './images/item-3.jpeg',
     desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
   },
   {
     id: 4,
-    title: "country delight",
-    category: "breakfast",
+    title: 'country delight',
+    category: 'breakfast',
     price: 20.99,
-    img: "./images/item-4.jpeg",
+    img: './images/item-4.jpeg',
     desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
   },
   {
     id: 5,
-    title: "egg attack",
-    category: "lunch",
+    title: 'egg attack',
+    category: 'lunch',
     price: 22.99,
-    img: "./images/item-5.jpeg",
+    img: './images/item-5.jpeg',
     desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
   },
   {
     id: 6,
-    title: "oreo dream",
-    category: "shakes",
+    title: 'oreo dream',
+    category: 'shakes',
     price: 18.99,
-    img: "./images/item-6.jpeg",
+    img: './images/item-6.jpeg',
     desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
   },
   {
     id: 7,
-    title: "bacon overflow",
-    category: "breakfast",
+    title: 'bacon overflow',
+    category: 'breakfast',
     price: 8.99,
-    img: "./images/item-7.jpeg",
+    img: './images/item-7.jpeg',
     desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
   },
   {
     id: 8,
-    title: "american classic",
-    category: "lunch",
+    title: 'american classic',
+    category: 'lunch',
     price: 12.99,
-    img: "./images/item-8.jpeg",
+    img: './images/item-8.jpeg',
     desc: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `,
   },
   {
     id: 9,
-    title: "quarantine buddy",
-    category: "shakes",
+    title: 'quarantine buddy',
+    category: 'shakes',
     price: 16.99,
-    img: "./images/item-9.jpeg",
+    img: './images/item-9.jpeg',
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
-];
+  {
+    id: 10,
+    title: 'steak dinner',
+    category: 'dinner',
+    price: 66.99,
+    img: './images/item-10.jpeg',
+    desc: `Live-edge lyft af, edison bulb yuccie crucifix microdosing. Skateboard fam synth authentic semiotics. `,
+  },
+]
+
+const items = document.querySelector('.items')
+const btnsContainer = document.querySelector('.btns')
+window.addEventListener('DOMContentLoaded', () => {
+  display(menu)
+  displaybtn()
+
+  const btns = document.querySelectorAll('.btn-menu')
+  btns.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+      let filter = menu.filter((item) => {
+        if (item.category === e.currentTarget.dataset.id) {
+          return item
+        }
+      })
+
+      if (e.currentTarget.dataset.id === 'all') {
+        display(menu)
+      } else {
+        display(filter)
+      }
+    })
+  })
+})
+
+function displaybtn() {
+  const categories = menu.reduce(
+    (values, item) => {
+      if (!values.includes(item.category)) {
+        values.push(item.category)
+      }
+      return values
+    },
+    ['all']
+  )
+  let displaybtn = categories.map((item) => {
+    return `<button class="btn btn-menu" data-id="${item}">${item}</button>`
+  })
+  displaybtn = displaybtn.join('')
+  btnsContainer.innerHTML = displaybtn
+}
+
+function display(menu) {
+  let displaymenu = menu.map((item) => {
+    return `<article class="item">
+          <div class="pic-container">
+            <img src="${item.img}" alt="pancake" class="img" />
+          </div>
+          <div class="info">
+            <div class="food">
+              <h5 class="item-name">${item.title}</h5>
+              <h5 class="price">&dollar;${item.price}</h5>
+            </div>
+            <p class="descr">
+              ${item.desc}
+            </p>
+          </div>
+        </article>`
+  })
+  displaymenu = displaymenu.join('')
+  items.innerHTML = displaymenu
+}
