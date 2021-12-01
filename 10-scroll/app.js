c4e780905e8a7ba424a4a9b8345e7d264fdcf793
Haoran Zhou <kens0serious@gmail.com>
2021-12-01 16:25:46 -0500

    Add smooth scroll and fixed navbar

--- 10-scroll/app.js
+++ 10-scroll/app.js
@@ -4,10 +4,66 @@
 //offsetTop - A Number, representing the top position of the element, in pixels
 
 // ********** set date ************
+const year = document.querySelector('#date')
+const date = new Date()
+year.textContent = date.getFullYear()
 
 // ********** close links ************
+const navbtn = document.querySelector('.nav-btn')
+const linksContainer = document.querySelector('.links-container')
+const links = document.querySelector('.links')
+
+navbtn.addEventListener('click', () => {
+  const containerHeight = linksContainer.getBoundingClientRect().height
+  const linksHeight = links.getBoundingClientRect().height
+
+  if (containerHeight === 0) {
+    linksContainer.style.height = `${linksHeight}px`
+    console.log(linksContainer.style.height)
+  } else {
+    linksContainer.style.height = 0
+  }
+})
 
 // ********** fixed navbar ************
+const navbar = document.querySelector('nav')
+
+window.addEventListener('scroll', () => {
+  let navheight = nav.getBoundingClientRect().height
+  if (window.scrollY > navheight) {
+    navbar.classList.add('fixed-nav')
+  } else {
+    navbar.classList.remove('fixed-nav')
+  }
+})
 
 // ********** smooth scroll ************
 // select links
+const scrollLinks = document.querySelectorAll('.scroll-link')
+
+scrollLinks.forEach((link) => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault()
+    const id = e.currentTarget.getAttribute('href').slice(1)
+    const element = document.getElementById(id)
+
+    const navHeight = navbar.getBoundingClientRect().height
+    const containerHeight = linksContainer.getBoundingClientRect().height
+
+    const fixedNav = navbar.classList.contains('fixed-nav')
+    let position = element.offsetTop - navHeight
+    if (!fixedNav) {
+      position = position - navHeight
+    }
+
+    if (navHeight > 63.98) {
+      position = position + containerHeight
+    }
+
+    window.scrollTo({
+      left: 0,
+      top: position,
+    })
+    linksContainer.style.height = 0
+  })
+})
