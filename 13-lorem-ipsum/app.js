5c1caf1029977adb34a3ff7238fd5bad1b66e6ce
Haoran Zhou <kens0serious@gmail.com>
2021-12-03 19:46:24 -0500

    Done

--- 13-lorem-ipsum/app.js
+++ 13-lorem-ipsum/app.js
@@ -10,4 +10,27 @@ I just told you! You've killed me! Fry! Quit doing the right thing, you jerk! Mi
   `Salvia glossier subway tile, leggings mustache YOLO semiotics chia. Pitchfork tbh af blog church-key meggings vaporware PBR&B master cleanse post-ironic man bun pabst mustache letterpress synth. Snackwave raw denim godard, 3 wolf moon shaman offal kitsch unicorn live-edge selvage schlitz fashion axe vaporware drinking vinegar prism. Shabby chic tacos artisan, chambray chicharrones cardigan leggings typewriter af pop-up williamsburg meditation PBR&B viral. You probably haven't heard of them DIY jean shorts subway tile fashion axe bushwick kitsch tumeric cloud bread vaporware freegan franzen pork belly chicharrones banh mi.`,
   `Man braid celiac synth freegan readymade, pitchfork fam salvia waistcoat lomo bitters gentrify four loko. Pitchfork semiotics post-ironic vegan. Tofu meditation microdosing hashtag semiotics venmo. Flexitarian vape tilde taiyaki. Prism poutine farm-to-table, messenger bag vegan taxidermy tattooed sartorial squid jean shorts fixie selvage trust fund vape.`,
   `Rutters Plate Fleet boom chandler Brethren of the Coast handsomely lookout marooned brigantine knave. Buccaneer gangway jack rum loot spyglass line Jack Tar fore gaff. Gaff topmast scuttle ballast swab draught measured fer yer chains dance the hempen jig Chain Shot yardarm.`,
-];
+]
+
+const form = document.querySelector('.text-form')
+const paragraph = document.querySelector('.text')
+const number = document.querySelector('#number')
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  let value = parseInt(number.value)
+  let random = Math.floor(Math.random(0, 1) * text.length)
+
+  if (isNaN(value) || value > 9 || value <= 0) {
+    paragraph.innerHTML = `<p>${text[random]}</p>`
+  } else {
+    let tempText = text
+      .slice(0, value)
+      .map((p) => {
+        return `<p>${p}</p>`
+      })
+      .join('')
+    paragraph.innerHTML = tempText
+  }
+})
